9312395d9a2fbee2dc57eadbcb33a27e176fefb8
conleyc86 <conleyc86@gmail.com>
2024-11-14 12:18:54 -0700

    Reverted layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
-import { useEffect } from 'react';
 
 export const metadata = {
     title: {
@@ -11,27 +10,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-    useEffect(() => {
-        const initMunchkin = () => {
-            if (!window.didInit) {
-                window.didInit = true;
-                window.Munchkin.init('509-TVZ-333', { altIds: ['168-CRJ-586'] });
-            }
-        };
-
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = '//munchkin.marketo.net/munchkin.js';
-        script.onload = initMunchkin;
-        script.onreadystatechange = function () {
-            if (this.readyState === 'complete' || this.readyState === 'loaded') {
-                initMunchkin();
-            }
-        };
-        document.head.appendChild(script);
-    }, []);
-
     return (
         <html lang="en" data-theme="lofi">
             <head>
