79d8de68a779c96a44dd9f419b73f698a4c78bc0
conleyc86 <conleyc86@gmail.com>
2024-11-14 11:01:09 -0700

    Update layout.jsx
    took chatgpt's advice on handling scripting on build

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import { useEffect } from 'react';
 
 export const metadata = {
     title: {
@@ -10,32 +11,31 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+    useEffect(() => {
+        const initMunchkin = () => {
+            if (!window.didInit) {
+                window.didInit = true;
+                window.Munchkin.init('509-TVZ-333', { altIds: ['168-CRJ-586'] });
+            }
+        };
+
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = '//munchkin.marketo.net/munchkin.js';
+        script.onload = initMunchkin;
+        script.onreadystatechange = function () {
+            if (this.readyState === 'complete' || this.readyState === 'loaded') {
+                initMunchkin();
+            }
+        };
+        document.head.appendChild(script);
+    }, []);
+
     return (
         <html lang="en" data-theme="lofi">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
-                <script type="text/javascript">
-                (function() {
-                  var didInit = false;
-                  function initMunchkin() {
-                    if(didInit === false) {
-                      didInit = true;
-                      Munchkin.init('509-TVZ-333', { altIds:['168-CRJ-586'] });
-                    }
-                  }
-                  var s = document.createElement('script');
-                  s.type = 'text/javascript';
-                  s.async = true;
-                  s.src = '//munchkin.marketo.net/munchkin.js';
-                  s.onreadystatechange = function() {
-                    if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                      initMunchkin();
-                    }
-                  };
-                  s.onload = initMunchkin;
-                  document.getElementsByTagName('head')[0].appendChild(s);
-                })();
-                </script>
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
