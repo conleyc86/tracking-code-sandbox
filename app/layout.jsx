727f25333d31871db9370fe078ca931dd5682288
conleyc86 <conleyc86@gmail.com>
2024-11-18 13:48:04 -0700

    Reverted layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
-import { useEffect } from 'react';
 
 export const metadata = {
     title: {
@@ -11,12 +10,12 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
     return (
         <html lang="en" data-theme="lofi">
-            <Head>
+            <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
-            </Head>
+            </head>
+            <body className="text-white bg-blue-900">
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
