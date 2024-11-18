37d6c271471e423ce24bfdfcafac066961908c0f
conleyc86 <conleyc86@gmail.com>
2024-11-18 13:39:55 -0700

    Cleaned layout.jsx again

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,33 +12,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-    useEffect(() => {
-        // Add the GTM script to the head dynamically
-        const script = document.createElement('script');
-        script.innerHTML = `
-            (function() {
-              var didInit = false;
-              function initMunchkin() {
-                if(didInit === false) {
-                  didInit = true;
-                  Munchkin.init('509-TVZ-333', { altIds:['168-CRJ-586'] });
-                }
-              }
-              var s = document.createElement('script');
-              s.type = 'text/javascript';
-              s.async = true;
-              s.src = '//munchkin.marketo.net/munchkin.js';
-              s.onreadystatechange = function() {
-                if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                  initMunchkin();
-                }
-              };
-              s.onload = initMunchkin;
-              document.getElementsByTagName('head')[0].appendChild(s);
-            })();
-        `;
-        document.head.appendChild(script);
-    }, []);
 
     return (
         <html lang="en" data-theme="lofi">
